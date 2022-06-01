73c1a9d105d3846fc02097ff6b04f94955b85159
Aditya Sharma <adityasharmaa1008@gmail.com>
2022-06-01 14:12:01 +0530

    Implementing the steps, screens and BL for the feature file

--- src/test/java/com/znsio/e2e/screen/CartScreen.java
+++ src/test/java/com/znsio/e2e/screen/CartScreen.java
@@ -7,10 +7,12 @@ import com.znsio.e2e.tools.Visual;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.log4j.Logger;
 
+import java.util.List;
+
 import static com.znsio.e2e.runner.Runner.fetchDriver;
 import static com.znsio.e2e.runner.Runner.fetchEyes;
 
-public class CartScreen {
+public abstract class CartScreen {
     private static final String SCREEN_NAME = LoginScreen.class.getSimpleName();
     private static final Logger LOGGER = Logger.getLogger(SCREEN_NAME);
 
@@ -18,6 +20,18 @@ public class CartScreen {
         Driver driver = fetchDriver(Thread.currentThread().getId());
         Platform platform = Runner.fetchPlatform(Thread.currentThread().getId());
         LOGGER.info(SCREEN_NAME + ": Driver type: " + driver.getType() + ": Platform: " + platform);
+        Visual visually = fetchEyes(Thread.currentThread().getId());
+        switch (platform) {
+            case web:
+                return new CartScreenWeb(driver, visually);
+        }
         throw new NotImplementedException(SCREEN_NAME + " is not implemented in " + Runner.platform);
     }
+
+    public abstract boolean verifyContentAdd();
+
+    public abstract boolean verifyContentRemove();
+
+    public abstract String getCartCounter();
+
 }

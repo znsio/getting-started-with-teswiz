c7b5c7d189a5afa2afc904f8c5132f7e64cbdaaa
Anand Bagmar <abagmar@gmail.com>
2022-07-12 15:02:51 +0530

    fixed locators for TheApp apk

--- src/test/java/com/znsio/sample/e2e/screen/android/theapp/EchoScreenAndroid.java
+++ src/test/java/com/znsio/sample/e2e/screen/android/theapp/EchoScreenAndroid.java
@@ -11,9 +11,8 @@ public class EchoScreenAndroid
     private final Visual visually;
     private final String SCREEN_NAME = EchoScreenAndroid.class.getSimpleName();
     private final String byMessageInputAccessibilityId = "messageInput";
-    private final By bySaveMessageButtonXpath = By.xpath("//android.view.ViewGroup[@content-desc=\"messageSaveBtn\"]/android.widget.TextView");
-    private final By byGoBackToHomeScreenButtonXpath = By.xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget" + ".RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageButton");
+    private final By bySaveMessageButtonXpath = By.xpath("//android.widget.Button[@content-desc=\"messageSaveBtn\"]/android.widget.TextView");
+    private final By byGoBackToHomeScreenButtonXpath = By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate Up\"]");
 
     public EchoScreenAndroid(Driver driver, Visual visually) {
         this.driver = driver;

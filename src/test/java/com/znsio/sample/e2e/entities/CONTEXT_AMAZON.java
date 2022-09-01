c10849ceff8abc921dd88575762eb79d38de226e
abhishek86.singh <asraisingh@gmail.com>
2022-09-01 09:25:13 +0530

    -New 'PRODUCT_SELECTED' state added

--- src/test/java/com/znsio/sample/e2e/entities/CONTEXT_AMAZON.java
+++ src/test/java/com/znsio/sample/e2e/entities/CONTEXT_AMAZON.java
@@ -8,5 +8,6 @@ public class CONTEXT_AMAZON extends TEST_CONTEXT {
     public static final String PRODUCT_TITLE = "productTitle";
     public static final String PRODUCT_PRICE = "productPrice";
     public static final String PRODUCT_QUANTITY ="productQuantity";
+    public static final String PRODUCT_SELECTED = "productSelected";
 
 }

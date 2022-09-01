354ee7acd9d5cb90826ef7698989c76b6e708308
abhishek86.singh <asraisingh@gmail.com>
2022-09-01 09:29:51 +0530

    -HomePageBL - code refactored

--- src/test/java/com/znsio/sample/e2e/businessLayer/amazon/HomePageBL.java
+++ src/test/java/com/znsio/sample/e2e/businessLayer/amazon/HomePageBL.java
@@ -3,15 +3,13 @@ package com.znsio.sample.e2e.businessLayer.amazon;
 import com.context.TestExecutionContext;
 import com.znsio.e2e.entities.Platform;
 import com.znsio.e2e.runner.Runner;
-import com.znsio.sample.e2e.businessLayer.jiomeet.InAMeetingBL;
-import com.znsio.sample.e2e.entities.SAMPLE_TEST_CONTEXT;
+import com.znsio.sample.e2e.entities.CONTEXT_AMAZON;
 import com.znsio.sample.e2e.screen.amazon.HomePageScreen;
-import com.znsio.sample.e2e.screen.amazon.SearchPageScreen;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 
 public class HomePageBL {
-    private static final Logger LOGGER = Logger.getLogger(InAMeetingBL.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(HomePageBL.class.getName());
     private final SoftAssertions softly;
     private final TestExecutionContext context;
 
@@ -19,12 +17,11 @@ public class HomePageBL {
         long threadId = Thread.currentThread().getId();
         this.context = Runner.getTestExecutionContext(threadId);
         softly = Runner.getSoftAssertion(threadId);
-        String currentUserPersona = SAMPLE_TEST_CONTEXT.ME;
+        String currentUserPersona = CONTEXT_AMAZON.ME;
         Platform currentPlatform = Runner.platform;
       }
-    public SearchPageBL searchForAProduct(String productName) {
-
-        SearchPageScreen homePageScreen = HomePageScreen.get().searchProduct(productName);
-        return new SearchPageBL();
+    public HomePageBL searchForAProduct(String productName) {
+        HomePageScreen.get().searchProduct(productName);
+        return this;
     }
 }

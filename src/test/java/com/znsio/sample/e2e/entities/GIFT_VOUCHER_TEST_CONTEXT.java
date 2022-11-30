880a9ddd406a4c2de9ee9bc4ee6f6909a717d874
Aman Daharwal <mndaharwal4@gmail.com>
2022-11-30 16:18:12 +0530

    Added indigo gift voucher test context file

---
+++ src/test/java/com/znsio/sample/e2e/entities/GIFT_VOUCHER_TEST_CONTEXT.java
@@ -0,0 +1,21 @@
+package com.znsio.sample.e2e.entities;
+
+import com.znsio.e2e.entities.TEST_CONTEXT;
+
+public class GIFT_VOUCHER_TEST_CONTEXT extends TEST_CONTEXT {
+
+   public static final String MESSAGE = "message";
+
+   public static final String DELIVERY_OPTION = "deliveryOption";
+
+   public static final String TNC = "tnc";
+
+   public static final String FIRST_NAME = "firstname";
+
+   public static final String LAST_NAME = "lastname";
+
+   public static final String EMAIL = "email";
+
+   public static final String PHONE = "phone";
+
+}

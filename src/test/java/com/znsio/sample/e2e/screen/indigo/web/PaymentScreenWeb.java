0f9ae0d6d73cf206021ac06190a48f726d5cc9b6
Aman Daharwal <mndaharwal4@gmail.com>
2022-12-01 15:02:21 +0530

    Addressed review comments - add comment for amount variable

--- src/test/java/com/znsio/sample/e2e/screen/indigo/web/PaymentScreenWeb.java
+++ src/test/java/com/znsio/sample/e2e/screen/indigo/web/PaymentScreenWeb.java
@@ -2,13 +2,15 @@ package com.znsio.sample.e2e.screen.indigo.web;
 
 import com.znsio.e2e.tools.Driver;
 import com.znsio.e2e.tools.Visual;
+import com.znsio.sample.e2e.screen.indigo.GiftVoucherScreen;
 import com.znsio.sample.e2e.screen.indigo.HomeScreen;
 import com.znsio.sample.e2e.screen.indigo.PaymentScreen;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class PaymentScreenWeb extends PaymentScreen {
-
+    private static final Logger LOGGER = Logger.getLogger(GiftVoucherScreen.class.getName());
     private final Driver driver;
     private final Visual visually;
     private final WebDriver innerDriver;
@@ -25,7 +27,9 @@ public class PaymentScreenWeb extends PaymentScreen {
     @Override
     public int getPaymentAmount() {
 
+        // Amount is in this format "INR 10000.00"
         String amount =  driver.waitTillElementIsPresent(byPaymentAmountXpath).getText();
-        return Integer.parseInt(amount.substring(4, amount.length() - 3));
+        LOGGER.info("Payment Amount is "+amount);
+        return Integer.parseInt(amount.split(" ")[1]);
     }
 }

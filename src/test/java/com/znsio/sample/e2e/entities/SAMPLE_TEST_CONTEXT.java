718cf272ac1079ddd8785b97bbfa642c3acc63f9
Mukund1 Gupta <Mukund1.Gupta@ril.com>
2023-06-16 17:58:45 +0530

    initial project setup files

--- src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
+++ src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
@@ -5,6 +5,7 @@ import com.znsio.teswiz.entities.TEST_CONTEXT;
 public class SAMPLE_TEST_CONTEXT
         extends TEST_CONTEXT {
     public static final String ME = "me";
+    public static final String RESTAURANT_NAME = "";
     public static final String MEETING_ID = "meetingId";
     public static final String MEETING_PASSWORD = "meetingPassword";
     public static final String INVITATION_LINK = "invitationLink";

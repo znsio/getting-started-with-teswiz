dd014360bf738282c25d1ed2909769780c826be9
Yatharth Pandya <Yatharth.Pandya@ril.com>
2023-06-20 10:22:19 +0530

    Comments removed

--- src/test/java/com/znsio/sample/e2e/steps/BookMyShow/BMSSteps.java
+++ src/test/java/com/znsio/sample/e2e/steps/BookMyShow/BMSSteps.java
@@ -16,24 +16,27 @@ import org.apache.log4j.Logger;
 public class BMSSteps {
     private static final Logger LOGGER = Logger.getLogger(BMSSteps.class.getName());
     private final TestExecutionContext context;
+
     public BMSSteps() {
         context = SessionContext.getTestExecutionContext(Thread.currentThread().getId());
         LOGGER.info("context: " + context.getTestName());
     }
+
     @Given("I, a guest user from {string} wants to see movie which is positioned at {string} on day after tomorrow")
     public void iAGuestUserFromWantsToSeeMovieWhichIsPositionedAtOnDayAfterTomorrow(String location, String moviePosition) {
         LOGGER.info(System.out.printf("iAGuestUserSearchForMovies - Persona:'%s', Platform: '%s'",SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()));
         Drivers.createDriverFor(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform(), context);
 
-        //new IndigoGiftVoucherBL(SAMPLE_TEST_CONTEXT.ME, Runner.platform).personaliseAndPreviewVoucherWithAmount(voucherDenomination,quantity);
         new BMSHomeBL(SAMPLE_TEST_CONTEXT.ME, Runner.getPlatform()).loginAsGuestUserForGivenLocation(location).selectMovieCategory();
         new BMSMoviesBL().selectMovie(moviePosition).selectDateAfter2Days();
     }
+
     @When("I select seats for {string} for the second last time slot in cinema hall present at position {string}")
     public void iSelectSeatsForForTheSecondLastTimeSlotInCinemaHallPresentAtPosition(String noOfSeats, String cinemaPosition) {
         new BMSMoviesBL().selectSecondLastTimeSlotForCinemaHallAtPosition(cinemaPosition).selectNoOfSeats(noOfSeats);
 
     }
+
     @Then("I check for availability of seats that should be more than {string} percent")
     public void iCheckForAvailabilityOfSeatsThatShouldBeMoreThanPercent(String validationPercentage) {
         new BMSSelectSeatsBL().validateAvailableSeatsPercentage(validationPercentage);

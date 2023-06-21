ccd5d92179544c4a3bdbf9278e06265b93757fdf
Mukund1 Gupta <Mukund1.Gupta@ril.com>
2023-06-21 15:50:07 +0530

    removed TIMESLOT_BOOKED context

--- src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
+++ src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
@@ -6,7 +6,6 @@ public class SAMPLE_TEST_CONTEXT
         extends TEST_CONTEXT {
     public static final String ME = "me";
     public static final String RESTAURANT_NAME = "";
-    public static final String TIMESLOT_BOOKED = "";
     public static final String MEETING_ID = "meetingId";
     public static final String MEETING_PASSWORD = "meetingPassword";
     public static final String INVITATION_LINK = "invitationLink";

1bcae0e18e6fd5383ac23509c4cb2524ef18b958
Anand Bagmar <abagmar@gmail.com>
2024-01-24 16:23:57 +0530

    Updated teswiz id (downgrade to appium java_client 8.6.0) Added new test for duckduckgo apk to test switching between native and webviews

---
+++ src/test/java/com/znsio/sample/e2e/businessLayer/duckduckgo/DuckDuckGoBL.java
@@ -0,0 +1,58 @@
+package com.znsio.sample.e2e.businessLayer.duckduckgo;
+
+import com.context.TestExecutionContext;
+import com.znsio.sample.e2e.entities.SAMPLE_TEST_CONTEXT;
+import com.znsio.sample.e2e.screen.duckduckgo.DuckDuckGoScreen;
+import com.znsio.teswiz.entities.Platform;
+import com.znsio.teswiz.runner.Runner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DuckDuckGoBL {
+    private static final Logger LOGGER = LogManager.getLogger(DuckDuckGoBL.class.getName());
+    private final TestExecutionContext context;
+    private final SoftAssertions softly;
+    private final String currentUserPersona;
+    private final Platform currentPlatform;
+
+    public DuckDuckGoBL(String userPersona, Platform forPlatform) {
+        long threadId = Thread.currentThread().getId();
+        this.context = Runner.getTestExecutionContext(threadId);
+        softly = Runner.getSoftAssertion(threadId);
+        this.currentUserPersona = userPersona;
+        this.currentPlatform = forPlatform;
+        Runner.setCurrentDriverForUser(userPersona, forPlatform, context);
+    }
+
+    public DuckDuckGoBL() {
+        long threadId = Thread.currentThread().getId();
+        this.context = Runner.getTestExecutionContext(threadId);
+        softly = Runner.getSoftAssertion(threadId);
+        this.currentUserPersona = SAMPLE_TEST_CONTEXT.ME;
+        this.currentPlatform = Runner.getPlatform();
+    }
+
+    public DuckDuckGoBL launchBrowser() {
+        DuckDuckGoScreen.get().launchBrowser();
+        return this;
+    }
+
+    public DuckDuckGoBL cancelChangingDefaultBrowserPopUp() {
+        DuckDuckGoScreen.get().cancelChangingDefaultBrowserPopup();
+        return this;
+    }
+
+    public DuckDuckGoBL switchToWebViewAndCheckDefaultText() {
+        String defaultTextFromWebView = DuckDuckGoScreen.get().getDefaultTextFromWebView();
+        assertThat(defaultTextFromWebView).as("Default text is empty").isNotEmpty();
+        return this;
+    }
+
+    public DuckDuckGoBL swithToNativeContextAndGoToTeswiz() {
+        DuckDuckGoScreen.get().switchToNativeContextAndGoToTeswizGithub();
+        return this;
+    }
+}

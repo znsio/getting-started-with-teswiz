12b79ce49157b1e50eb945944918e0bc8018b725
Anand Bagmar <abagmar@gmail.com>
2024-11-15 17:02:10 +0530

    updated to teswiz 1.0.6 Added AsyncCommandLineExecutor test scenario for testing an interactive CLI (calculator example)

--- src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
+++ src/test/java/com/znsio/sample/e2e/entities/SAMPLE_TEST_CONTEXT.java
@@ -10,5 +10,6 @@ public class SAMPLE_TEST_CONTEXT
     public static final String INVITATION_LINK = "invitationLink";
     public static final String GUEST_USER = "guestUser";
     public static final String INITIAL_ELEMENT_ID = "elementId";
-
+    public static final String CLI_COMMAND_NUMBER = "cliCommandNumber";
+    public static final String ASYNC_COMMAND_LINE_EXECUTOR = "asyncCommandLineExecutor";
 }

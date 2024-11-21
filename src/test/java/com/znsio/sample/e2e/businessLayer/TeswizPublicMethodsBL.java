ab374ecc9a7ebda335306f262004310f1b3be4b6
Anand Bagmar <abagmar@gmail.com>
2024-11-21 16:23:27 +0530

    updated teswizPublicMethodsCheck

--- src/test/java/com/znsio/sample/e2e/businessLayer/TeswizPublicMethodsBL.java
+++ src/test/java/com/znsio/sample/e2e/businessLayer/TeswizPublicMethodsBL.java
@@ -5,13 +5,15 @@ import com.znsio.sample.e2e.entities.SAMPLE_TEST_CONTEXT;
 import com.znsio.teswiz.entities.Platform;
 import com.znsio.teswiz.runner.Drivers;
 import com.znsio.teswiz.runner.Runner;
-import com.znsio.teswiz.tools.ReportPortalLogger;
+import com.znsio.teswiz.services.UnirestService;
+import com.znsio.teswiz.tools.*;
 import io.cucumber.java.Scenario;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.assertj.core.api.SoftAssertions;
 
 import java.io.File;
+import java.util.HashMap;
 
 public class TeswizPublicMethodsBL {
     private static final Logger LOGGER = LogManager.getLogger(TeswizPublicMethodsBL.class.getName());
@@ -37,47 +39,58 @@ public class TeswizPublicMethodsBL {
         this.currentPlatform = Runner.getPlatform();
     }
 
+//    @Test
     private void teswizPublicMethodsCheck() {
         long threadId = Thread.currentThread().getId();
+
+        String osName = Runner.OS_NAME;
+        boolean isWindows = Runner.IS_WINDOWS;
+        boolean isMac = Runner.IS_MAC;
+        String userDirectory = Runner.USER_DIRECTORY;
+        String userName = Runner.USER_NAME;
+        String notSet = Runner.NOT_SET;
+        String aDefault = Runner.DEFAULT;
+        String debug = Runner.DEBUG;
+        String info = Runner.INFO;
+        String warn = Runner.WARN;
 
         new Runner();
         new Runner("configFilePath", "stepDefDirName", "featuresDirName");
         Runner.getPlatformForUser("me");
-        Runner.shouldFailTestOnVisualDifference();
-        Runner.getTestExecutionContext(1);
-
+        Runner.getHostName();
         Runner.getPlatform();
         Runner.getApplitoolsConfiguration();
-        Runner.getCloudKey();
         Runner.getCloudName();
         Runner.getCloudUser();
-
+        Runner.getCloudKey();
         Runner.getRemoteDriverGridPort();
+        Runner.getRemoteDriverGridHostName();
         Runner.getMaxNumberOfAppiumDrivers();
         Runner.getMaxNumberOfWebDrivers();
         Runner.isVisualTestingEnabled();
+        Runner.shouldFailTestOnVisualDifference();
         Runner.getFromEnvironmentConfiguration("BASE_URL");
-
         Runner.getTestData("USERNAME");
         Runner.getTestDataAsMap("ENV");
+        Runner.main(new String[]{});
+        Runner.getTestExecutionContext(1);
         Runner.getSoftAssertion(threadId);
         Runner.setCurrentDriverForUser(currentUserPersona, currentPlatform, context);
         Runner.fetchPlatform(threadId);
-
         Runner.getTargetEnvironment();
         Runner.getBaseURLForWeb();
         Runner.getAppPackageName();
         Runner.isRunningInCI();
+        Runner.isCLI();
+        Runner.isAPI();
         Runner.getBrowser();
-
         Runner.getProxyURL();
         Runner.getBrowserConfigFileContents();
         Runner.getBrowserConfigFileContents("non-default-browserConfig.json");
         Runner.getBrowserConfigFile();
-        Runner.getRemoteDriverGridHostName();
-        Runner.getHostName();
 
         Drivers.setDriverFor(currentUserPersona, currentPlatform, context);
+        Drivers.isDriverAssignedForUser(currentUserPersona);
         Drivers.createDriverFor(currentUserPersona, currentPlatform, context);
         Drivers.createDriverFor(currentUserPersona, "appName", "browserName", currentPlatform,
                                 context);
@@ -87,7 +100,6 @@ public class TeswizPublicMethodsBL {
         Drivers.getDriverForCurrentUser(threadId);
         Drivers.getVisualDriverForCurrentUser(threadId);
         Drivers.getNameOfDeviceUsedByUser(currentUserPersona);
-        Drivers.isDriverAssignedForUser(currentUserPersona);
         Drivers.getAvailableUserPersonas();
         Scenario scenario = null;
         Drivers.attachLogsAndCloseAllDrivers(scenario);
@@ -96,5 +108,41 @@ public class TeswizPublicMethodsBL {
         ReportPortalLogger.logInfoMessage("logInfoMessage");
         ReportPortalLogger.logWarningMessage("logWarningMessage");
         ReportPortalLogger.attachFileInReportPortal("attachFileInReportPortal", new File("build.gradle"));
+
+        UnirestService.getHttpResponse("");
+        UnirestService.getHttpResponseWithQueryParameter("", "", "");
+        UnirestService.getHttpResponseWithQueryMap("", new HashMap<>());
+        UnirestService.postHttpRequest("", "");
+        UnirestService.patchHttpRequest("", "");
+        UnirestService.deleteHttpRequest("");
+
+        DateTime.getFormattedMeetingTime(5);
+
+        IPAddress.getPublicIPAddress();
+        IPAddress.getPrivateIPAddress();
+
+        JsonFile.saveJsonToFile(new HashMap<>(), "");
+        JsonFile.getNodeValueAsMapFromJsonFile("", "");
+        JsonFile.loadJsonFile("");
+        String [] a = new String[0];
+        JsonFile.getNodeValueAsStringFromJsonFile("", a);
+        JsonFile.getValueFromLoadedJsonMap("", a, new HashMap<>());
+        JsonFile.getNodeValueAsArrayListFromJsonFile("", "");
+        JsonFile.convertToMap("");
+        JsonFile.convertToArray("");
+        JsonFile.compareFiles("", "");
+
+        JsonSchemaValidator.validateJsonFileAgainstSchema("", "", "");
+
+        Randomizer.randomize(5);
+        Randomizer.randomize("5");
+        Randomizer.randomizeAlphaNumericString(5);
+        Randomizer.randomizeString(5);
+        Randomizer.getRandomNumberBetween(1, 2);
+        Randomizer.getRandomNumberBetween(1L, 2L);
+
+        Wait.waitFor(1);
+
+        YamlFile.compareFiles("", "");
     }
 }
